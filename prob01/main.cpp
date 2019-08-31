ac27de999f40f85114bc730f083645346b32eb10
manuelito013101 <manuelito@csu.fullerton.edu>
2019-08-31 13:59:19 -0700

    this is the diamond patter

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,16 @@
-// Name: 
+// Name:
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
-
+std::cout<<"     *\n";
+std::cout<<"    ***\n";
+std::cout<<"   *****\n";
+std::cout<<"  *******\n";
+std::cout<<"   *****\n";
+std::cout<<"    ***\n";
+std::cout<<"     *\n";                
   return 0;
 }
