86cfc28ed012b6f45d5438c6e394c20a14f7df02
manuelito013101 <manuelito@csu.fullerton.edu>
2019-08-31 15:05:35 -0700

    total bill cost

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,32 @@
-// Name: 
+// Name:
 // This program calculates the tax and tip on a restaurant bill.
+
+#include <iostream>
+
+int main()
+{
+  double cost,tax,tip;
+  double total;
+
+//get thye cost of the meal
+  std::cout << "What is the total \"cost\" of the meal? ";
+  std::cin >> cost;
+
+  //what is the tax of the meal?
+  tax = cost * .0775;
+  std::cout << "The \"tax\" of the cost is"<<tax<<" dollars./n";
+
+//calculate the tip.
+tip= cost * .2;
+
+//display the tip.
+std::cout << "The \"tip\" of the cost is"<<tip<<" dollars./n";
+
+// calculate the total
+total= cost+tax+tip;
+
+//display total
+std::cout << "The \"total\" of the cost is"<<total<<" dollars./n";
+
+  return 0;
+}
